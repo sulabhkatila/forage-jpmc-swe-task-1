77578c911e0ecc2da0b3ce149702c3668304a20d
Sulabh Katila <sulabhkatila@gmail.com>
2023-07-05 13:56:50 -0400

    Task 1 complete

--- client_test.py
+++ client_test.py
@@ -7,18 +7,28 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+
+    # Assertions
+    stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+
+    self.assertEqual(stock, 'ABC')
+    self.assertAlmostEqual(bid_price, 120.48)
+    self.assertAlmostEqual(ask_price, 121.2)
+    self.assertAlmostEqual(price, 120.84)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+    
+    # Assertions
+    stock, bid_price, ask_price, price = getDataPoint(quotes[1])
 
-
-  """ ------------ Add more unit tests ------------ """
-
+    self.assertEqual(stock, 'DEF')
+    self.assertAlmostEqual(bid_price, 117.87)
+    self.assertAlmostEqual(ask_price, 121.68)
+    self.assertAlmostEqual(price, 119.775)
 
 
 if __name__ == '__main__':
